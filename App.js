9cc090cec299b769646eb8ed47004b345b152f2b
Mi6u3l <miguelbragagomes@gmail.com>
2019-07-31 21:49:39 +0100

    adds gps test

--- App.js
+++ App.js
@@ -1,19 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet, Button } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>UCD ASE React Native energy consumption test app</Text>
+export default class App extends Component {
+  state = {
+    location: null,
+    feedBackMessage: null,
+  };
+
+  startGPSTest = () => {
+    let intervals = 0;
+    let interval = setInterval(() => {
+      intervals++;
+      if (intervals < 1000) {
+        this.getLocation();  
+      } else {
+        clearInterval(interval);
+        this.setState({
+          feedBackMessage: 'Test completed',
+        });
+      }
+      
+    }, 2000);
+  }
+
+  getLocation = () => {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        feedBackMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        feedBackMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  render() {
+    let text = '';
+    if (this.state.feedBackMessage) {
+      text = this.state.feedBackMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text>UCD ASE React Native energy consumption test app</Text>
+      <Button
+        onPress={this.startGPSTest}
+        title="Start GPS test"
+      />
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{text}</Text>
+      </View>
     </View>
-  );
+
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
