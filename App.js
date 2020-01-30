0c91297fc0fc74f0cccef434aa2a261933a537a2
Mi6u3l <miguelbragagomes@gmail.com>
2020-01-30 22:21:25 +0000

    adds sound

--- App.js
+++ App.js
@@ -1,34 +1,64 @@
-import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet, Button, ToolbarAndroid } from 'react-native';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import React, { Component } from "react";
+import {
+  Platform,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  ToolbarAndroid,
+  Vibration
+} from "react-native";
+import { Audio } from "expo-av";
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import Torch from "react-native-torch";
 
 export default class App extends Component {
   state = {
     location: null,
     feedBackMessage: null,
+    torchOn: false
   };
 
   startGPSTest = () => {
-        this.getLocation();  
-  }
+    this.getLocation();
+  };
 
+  toggleFlashLight = () => {
+    Torch.switchState(true);
+  //  this.setState({ torchOn: !this.torchOn });
+  };
+
+  vibrate = () => {
+    Vibration.vibrate(2000);
+  };
+
+  playSound = () => {
+    const soundObject = new Audio.Sound();
+    soundObject.loadAsync(require('./assets/sounds/sound.mp3'))
+      .then(() => {
+        soundObject.playAsync(); 
+      });
+    
+  };
+  
   getLocation = () => {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
+    if (Platform.OS === "android" && !Constants.isDevice) {
       this.setState({
-        feedBackMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        feedBackMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
       });
     } else {
       this._getLocationAsync();
     }
-  }
+  };
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    if (status !== "granted") {
       this.setState({
-        feedBackMessage: 'Permission to access location was denied',
+        feedBackMessage: "Permission to access location was denied"
       });
     }
 
@@ -37,30 +67,51 @@ export default class App extends Component {
   };
 
   render() {
-    let text = '';
+    let text = "";
     if (this.state.feedBackMessage) {
       text = this.state.feedBackMessage;
     } else if (this.state.location) {
       let latitude = parseFloat(this.state.location.coords.latitude);
       let longitude = parseFloat(this.state.location.coords.longitude);
-      text = `Latitude ${latitude.toFixed(7)} Longitude: ${longitude.toFixed(7)}`;
+      text = `Latitude ${latitude.toFixed(7)} Longitude: ${longitude.toFixed(
+        7
+      )}`;
     }
 
     return (
       <View style={styles.container}>
-  <ToolbarAndroid
-            style={styles.toolbar}
-            title="UCD ASE React Native energy consumption test app"
-            />
-      <Button 
-        onPress={this.startGPSTest}
-        title="Get Current Location"
-      />
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
-      </View>
-    </View>
+        <ToolbarAndroid
+          style={styles.toolbar}
+          title="UCD ASE React Native energy consumption test app"
+        />
 
+        <Button
+          onPress={this.startGPSTest}
+          title="Get Current Location"
+          className="button"
+        />
+        <Text style={styles.text}></Text>
+        <Button
+          onPress={this.toggleFlashLight}
+          title="Toggle Flash Light"
+          className="button"
+        />
+        <Text style={styles.text}></Text>
+        <Button
+          onPress={this.vibrate}
+          title="Vibrate"
+          className="button"
+        />
+        <Text style={styles.text}></Text>
+        <Button
+          onPress={this.playSound}
+          title="Play sound"
+          className="button"
+        />
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>{text}</Text>
+        </View>
+      </View>
     );
   }
 }
@@ -68,21 +119,24 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1"
   },
   paragraph: {
     margin: 24,
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center"
   },
-   toolbar: {
-   backgroundColor: '#2196F3',
-   height: 56,
-   marginBottom: 10,
-   alignSelf: 'stretch',
-   textAlign: 'center',
- },
+  toolbar: {
+    backgroundColor: "#2196F3",
+    height: 56,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    textAlign: "center"
+  },
+  text: {
+    height: 10
+  }
 });
