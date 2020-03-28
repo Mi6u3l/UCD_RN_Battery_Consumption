5b1a89d8648d2761a974e27c1332c72b78d849a0
Mi6u3l <miguelbragagomes@gmail.com>
2020-03-28 21:42:12 +0000

    refactor

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends Component {
 
   toggleFlashLight = () => {
     Torch.switchState(true);
-  //  this.setState({ torchOn: !this.torchOn });
+    this.setState({ torchOn: !this.torchOn });
   };
 
   vibrate = () => {
